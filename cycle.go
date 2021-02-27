23567992cbb867f38b23e031a0015b038b5b0ab3
Changkun Ou <hi@changkun.de>
2021-02-27 18:05:48 +0100

    v0.4.0

--- cycle.go
+++ cycle.go
@@ -8,9 +8,9 @@ import (
 
 // Call prints package version
 func Call() {
-	fmt.Printf("cyclemod@%s depends cyclemod2%s\n",
+	fmt.Printf("cyclemod@%s depends cyclemod2@%s\n",
 		Version, cyclemod2.Version)
 }
 
 // Version is the version of the module
-var Version = "v0.3.0"
+var Version = "v0.4.0"
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module changkun.de/x/cyclemod
 
 go 1.16
 
-require changkun.de/x/cyclemod2 v0.2.0
+require changkun.de/x/cyclemod2 v0.3.0
